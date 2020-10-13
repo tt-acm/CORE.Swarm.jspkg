f013023d883fbdeed2097f2bd32507b800a047d0
hanshenSun <hanshensunw@gmail.com>
2020-10-13 17:53:28 -0400

    typo fix

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ class SwarmApp {
       .then((res) => {
         // console.log(`statusCode: ${res.statusCode}`);
         console.log(res.data.values);
-        console.log(res.data.values.InnerTree['{ 0; }']);
+        console.log(res.data.values[0].InnerTree['{ 0; }']);
         return res;
       })
       .catch((error) => {
