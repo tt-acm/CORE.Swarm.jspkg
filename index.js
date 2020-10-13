6152d3859947d2cc85b214efea888c93137df7f2
hanshenSun <hanshensunw@gmail.com>
2020-10-13 10:02:23 -0400

    using CommonJS syntax

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-export default class SwarmCompute {
+class SwarmCompute {
   constructor(units, tolerance) {
     this.units = units;
     this.tolerance = tolerance;
   }
 }
+
+
+module.exports = SwarmCompute;
