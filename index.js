aac0378bf03b3c9c04248fe77d864f30ddeb8c53
hanshenSun <hanshensunw@gmail.com>
2020-10-13 10:30:40 -0400

    updating constructors

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 class SwarmCompute {
   constructor(units, tolerance) {
+    this.document = null;
+    this.definition = null;
+  }
+
+  // Method
+  setDocument(units, tolerance) {
+    this.document = new Document(units, tolerance);
+  }
+}
+
+class Document {
+  constructor(units, tolerance) {
     this.units = units;
     this.tolerance = tolerance;
   }
