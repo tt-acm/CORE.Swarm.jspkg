71e5b4ea60b6b6f7a8acb295f2051449a1c708b7
Benjamin Howes <bhowes@thorntontomasetti.com>
2020-10-13 22:46:02 -0400

    trying to remove express
    and messing with npm package updates

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const express = require('express');
 const axios = require('axios');
-const app = express();
-const port = process.env.PORT || 5000;
-
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({
-    express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'
-  });
-});
 
 const typeDict = {
   101: "Boolean",
