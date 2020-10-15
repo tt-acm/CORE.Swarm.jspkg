9da9b472b97496263b602f5c5f7f37d27395c2f9
hanshenSun <hanshensunw@gmail.com>
2020-10-15 11:14:50 -0400

    debugging

--- index.js
+++ index.js
@@ -150,6 +150,7 @@ class SwarmApp {
                 "DisplayColor": ""
               }
             };
+            console.log("currentGeo", currentGeo);
 
             tree.push(currentGeo);
           });
