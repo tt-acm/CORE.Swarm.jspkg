a5be6fd0a745358d8518ced646589171543a1913
hanshenSun <hanshensunw@gmail.com>
2020-10-15 14:44:52 -0400

    testing

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ class SwarmApp {
             const currentGeo = {
               type: "Rhino.Geometry.Point3d",
               // data: JSON.stringify(element),
-              data: "{\"version\":10000,\"archive3dm\":60,\"opennurbs\":-1912572244,\"data\":\"+n8CAFUAAAAAAAAA+/8CABQAAAAAAAAAHRoQw1fx0xG/5wAQgwEi8ExUB6T8/wIAHQAAAAAAAAAQZmZmZmbmMkAAAAAAAIA2wDMzMzMzMwdAUhNs2/9/AoAAAAAAAAAAAA==\"}",
+              data: "{\'version\':10000,\'archive3dm\':60,\'opennurbs\':-1912572244,\'data\':\'+n8CAFUAAAAAAAAA+/8CABQAAAAAAAAAHRoQw1fx0xG/5wAQgwEi8ExUB6T8/wIAHQAAAAAAAAAQZmZmZmbmMkAAAAAAAIA2wDMzMzMzMwdAUhNs2/9/AoAAAAAAAAAAAA==\'}",
               attributes: {
                 "Name": null,
                 "LayerName": null,
